dfdaa1943e647ae98abe9d518389677da863a831
Erin Noyes <emnoyes@gmail.com>
2018-03-27 20:23:02 -0700

    Assignment 19 CRUD2: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,30 @@ require 'random_data'
 
 
 50.times do
-    
     Post.create!(
-                 
-                 title:  RandomData.random_sentence,
-                 body:   RandomData.random_paragraph
-                 )
+         title:  RandomData.random_sentence,
+          body:   RandomData.random_paragraph
+)
 end
 posts = Post.all
 
 
 100.times do
     Comment.create!(
-                    
-                    post: posts.sample,
-                    body: RandomData.random_paragraph
-                    )
+        post: posts.sample,
+        body: RandomData.random_paragraph
+)
+end
+
+100.times do
+    Question.create!(
+       title: RandomData.random_sentence,
+       body: RandomData.random_paragraph,
+       resolved: false
+)
 end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
