dd1e8c1046950bad4151ae8168d4a2fd350cb023
Erin Noyes <emnoyes@gmail.com>
2018-03-27 20:55:13 -0700

    Assignment 18 CRUD: updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,31 @@ require 'random_data'
 
 
 50.times do
-    
     Post.create!(
-                 
-                 title:  RandomData.random_sentence,
-                 body:   RandomData.random_paragraph
-                 )
+      title:  RandomData.random_sentence,
+      body:   RandomData.random_paragraph
+)
 end
 posts = Post.all
 
 
 100.times do
     Comment.create!(
-                    
-                    post: posts.sample,
-                    body: RandomData.random_paragraph
-                    )
+      title:  RandomData.random_sentence,
+      copy:   RandomData.random_paragraph,
+      price:  RandomData.random_int
+)
+end
+
+20.times do
+Advertisement.create!(
+      title:  RandomData.random_sentence,
+      copy:   RandomData.random_paragraph,
+      price:  RandomData.random_int
+)
 end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
