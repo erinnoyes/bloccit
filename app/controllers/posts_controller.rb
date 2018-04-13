123ec68256b76ec32dee356f61b30bb1dac6247f
Erin Noyes <emnoyes@gmail.com>
2018-04-13 06:00:32 -0700

    Checkpoint 26 PostsUsers: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 
 class PostsController < ApplicationController
+    
+  before_action :require_sign_in, except: :show
+
 
   def show
       @post = Post.find(params[:id])
@@ -11,14 +14,10 @@ class PostsController < ApplicationController
   end
   
   def create
-      
-      @post = Post.new
-      @post.title = params[:post][:title]
-      @post.body = params[:post][:body]
       @topic = Topic.find(params[:topic_id])
+      @post = @topic.posts.build(post_params)
+      @post.user = current_user
 
-      @post.topic = @topic
-      
       if @post.save
           
           flash[:notice] = "Post was saved."
@@ -36,8 +35,7 @@ class PostsController < ApplicationController
   
   def update
       @post = Post.find(params[:id])
-      @post.title = params[:post][:title]
-      @post.body = params[:post][:body]
+      @post.assign_attributes(post_params)
       
       if @post.save
           flash[:notice] = "Post was updated."
@@ -58,6 +56,11 @@ class PostsController < ApplicationController
           flash.now[:alert] = "There was an error deleting the post."
           render :show
       end
+  end
+  
+  private
+  def post_params
+      params.require(:post).permit(:title, :body)
   end
 
 end
