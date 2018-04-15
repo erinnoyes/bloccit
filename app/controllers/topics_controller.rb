4446a198508f0354f7c352cab36d7660f85f0f21
Erin Noyes <emnoyes@gmail.com>
2018-04-15 12:26:19 -0700

    Assignment 27 Authorization updated

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
 
     before_action :authorize_user, except: [:index, :show]
     
-    before_action :authorize_moderator, only: [:edit, :update]
+    before_action :authorize_moderator, only: [:new, :create]
     
     
 
